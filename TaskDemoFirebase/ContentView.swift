48f4e6fd5e9e68a94d41433f02c4429340d1c95e
Johnny Hacking <jhacking@bhico.com>
2022-04-02 15:08:24 -0600

    Initial Commit

---
+++ TaskDemoFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskDemoFirebase
+//
+//  Created by Johnny Hacking on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskDemoFirebase/TaskDemoFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDemoFirebaseApp.swift
+//  TaskDemoFirebase
+//
+//  Created by Johnny Hacking on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskDemoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
